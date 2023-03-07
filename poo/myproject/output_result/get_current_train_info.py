beeecc8d9820b40fb48c35e7cd58299bd4da97ff
Keunji <nemonedo1@naver.com>
2023-03-07 14:31:22 +0900

    UPDATE get_current_train_info.py
    역번호 기반으로도 몇개 역 전에 있는지 알 수 있도록 변경하였습니다.

--- poo/myproject/output_result/get_current_train_info.py
+++ poo/myproject/output_result/get_current_train_info.py
@@ -1,15 +1,44 @@
 import requests
 import re
+import sys
+import os
+import sqlite3
+from dotenv import load_dotenv
 
-def get_current_location(MSG):
-    if ('[' in MSG) & ('(' in MSG):
-        num = re.search('(?=\[).+(?=\])', MSG).group().lstrip('[')
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+load_dotenv()
+
+CERTIFICATION_KEY = os.environ['TRAIN_KEY']
+
+
+def get_station_number(STATION_NAME):
+    conn = sqlite3.connect('../result.db')
+    c = conn.cursor()
+    c.execute(f'SELECT * FROM station WHERE stationname = :NAME', {'NAME': STATION_NAME})
+
+    station_number = c.fetchone()[2]
+    return int(station_number)
+
+def get_current_location(MSG, SEC, SEARCH_STATION_NUMBER):
+    if SEC == '0':
+        if ('[' in MSG) & ('(' in MSG):
+            num = re.search('(?<=\[).+(?=\])', MSG).group()
         
-    elif '전역' in MSG:
-        num = '1'
-        
+        elif '전역' in MSG:
+            num = '1'
+            
+        else:
+            num = '0'
+
     else:
-        num = '0'
+        if '(' in MSG:
+            station_name = re.findall('\(.+(?=\))', MSG)[0].lstrip('(')
+            trains_station_number = get_station_number(station_name)
+            search_station_number = int(SEARCH_STATION_NUMBER[-3:])
+            num = abs(trains_station_number - search_station_number)
+
+        else:
+            num = '0'
 
     return int(num)
         
@@ -64,7 +93,7 @@ def get_current_train_info(TRAIN_LIST, UPDNLINE = '하행'):
         if (train.get('subwayId') == '1001') & (train.get('updnLine') == UPDNLINE):
             Train_num = train.get('btrainNo')
             Destination = get_destination(train.get('bstatnNm'))
-            Current_Location_num = get_current_location(train.get('arvlMsg2'))
+            Current_Location_num = get_current_location(train.get('arvlMsg2'), train.get('barvlDt'), train.get('statnId'))
             Current_Location_stn = train.get('arvlMsg3')
             local_express_fx = lambda x : x if x == '급행' else '일반'
             local_express = local_express_fx(train.get('btrainSttus'))
@@ -95,12 +124,7 @@ def getCTI(CERTIFICATION_KEY, STATION_NAME, UPDNLINE = '하행'):
 
 if __name__ == '__main__':
 
-    train_list = get_arrival_train_list('5a556a59416e656d33384d4e754941', '가산디지털단지')
-    
-    try:
-        print(get_current_train_info(train_list, '하행'))
-
-    except:
-        print('입력 정보를 다시 확인하십시오.')
+    train_list = getCTI(CERTIFICATION_KEY, '종각', '하행')
+    print(train_list)
     
     
